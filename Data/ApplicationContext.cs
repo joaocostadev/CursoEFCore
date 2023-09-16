e946263073bea77a7e34cf38ce9112064fadd466
João Paulo <54214033+joaocostadev@users.noreply.github.com>
2023-09-15 22:54:41 -0300

    Métodos de persistências no DB - EF Core

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -1,14 +1,20 @@
-﻿using CursoEFCore.Data.Configurations;
-using CursoEFCore.Domain;
+﻿using CursoEFCore.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace CursoEFCore.Data
 {
     public class ApplicationContext : DbContext
     {
+        //private static readonly ILoggerFactory _logger = LoggerFactory.Create(p=>p.AddConsole());
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
         {
-            optionBuilder.UseSqlServer(
+            optionBuilder
+                //.UseLoggerFactory(_logger)
+                .UseSqlServer(
                 "Data source=(localdb)\\mssqllocaldb;Initial Catalog=CursoEFCore; Integrated Security=true");
         }
 
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using CursoEFCore.Domain;
+using CursoEFCore.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CursoEFCore
 {
@@ -6,7 +11,129 @@ namespace CursoEFCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RemoverRegistro();
+        }
+
+        private static void RemoverRegistro()
+        {
+            using var db = new Data.ApplicationContext();
+
+            var cliente = db.Clientes.Find(3);
+            //db.Clientes.Remove(cliente);
+            db.Remove(cliente);
+            //db.Entry(cliente).State = EntityState.Deleted;
+            db.SaveChanges();
+
+
+        }
+        private static void AtualizarPedidos()
+        {
+            using var db = new Data.ApplicationContext();
+            var cliente = db.Clientes.Find(1);
+
+            cliente.Nome = "Cliente Alterado 2";
+            //db.Clientes.Update(cliente);
+            db.SaveChanges();
+        }
+        private static void ConsultarPedidoCarregamentoAdiantado()
+        {
+            using var db = new Data.ApplicationContext();
+            var pedidos = db.Pedidos
+                .Include(p=>p.Itens)
+                .ThenInclude(p=>p.Produto)
+                .ToList();
+
+            Console.WriteLine(pedidos.Count);
+        }
+        private static void CadastrarPedido()
+        {
+            using var db = new Data.ApplicationContext();
+
+            var cliente = db.Clientes.FirstOrDefault();
+            var produto = db.Produtos.FirstOrDefault();
+
+            var pedido = new Pedido
+            {
+                ClienteId = cliente.Id,
+                IniciadoEm = DateTime.Now,
+                FinalizadoEm = DateTime.Now,
+                Observacao = "Pedido Teste",
+                Status = StatusPedido.Analise,
+                TipoFrete = TipoFrete.SemFrete,
+                Itens = new List<PedidoItem>
+                {
+                    new PedidoItem 
+                    {
+                        ProdutoId = produto.Id,
+                        Desconto = 0,
+                        Quantidade = 1,
+                        Valor = 10
+                    }
+                }
+            };
+
+            db.Pedidos.Add(pedido);
+
+            db.SaveChanges();
+
+        }
+        private static void ConsultaDados()
+        {
+            using var db = new Data.ApplicationContext();
+            //var consultaPorSintaxe = (from c in db.Clientes where c.Id> 0 select c).ToList();
+            var consultaPorMetodo = db.Clientes.Where(c => c.Id>0).ToList();
+            foreach(var cliente in consultaPorMetodo)
+            {
+                Console.WriteLine($"Consultando cliente: {cliente.Id}");
+                db.Clientes.Find(cliente.Id);
+            }
+        }
+        public static void InserirDadosEmMassa() 
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            var cliente = new Cliente
+            {
+                Nome = "João Paulo",
+                CEP = "49680000",
+                Cidade = "Glória",
+                Estado = "SE",
+                Telefone = "999985656"
+            };
+
+            using var db = new Data.ApplicationContext();
+            db.AddRange(produto, cliente);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total de Registro(s): {registros}");
+        }
+        public static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            using var db = new Data.ApplicationContext();
+            //db.Produtos.Add(produto); 
+            //db.Set<Produto>().Add(produto);
+            //db.Entry(produto).State = EntityState.Added;
+            db.Add(produto);
+
+            var registros = db.SaveChanges();
+
+            Console.WriteLine($"Total de Registro(s): {registros}");
         }
     }
 }
